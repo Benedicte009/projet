8c0e10b9b80b28de4a45e5ad04a87aada1bb3292
Benedicte009 <imbundabenedicte@gmail.com>
2026-06-30 13:10:29 +0100

    commit 3

--- fiches.php
+++ fiches.php
@@ -1,60 +1,86 @@
 <?php
+
 include "connexion_bd.php";
+
+
+$id_matiere = $_GET['id'];
+
+
+$sql = "SELECT * FROM fiches WHERE id_matiere='$id_matiere'";
+
+$resultat = mysqli_query($connexion,$sql);
+
+
 ?>
 
+
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 
 <head>
-<meta charset="UTF-8">
+
 <title>Fiches</title>
+
 <link rel="stylesheet" href="css/style_global.css">
+
 </head>
+
 
 <body>
 
-<header>
-<h1>Fiches de révision</h1>
 
-<nav>
-    <a href="index.php">Accueil</a>
-    <a href="matieres.php">Matières</a>
-    <a href="fiches.php">Fiches</a>
-    <a href="mes_fiches.php">Mes fiches</a>
-    <a href="connexion.php">Connexion</a>
-    <a href="inscription.php">Inscription</a>
-</nav>
-</header>
+<?php include "includes/header.php"; ?>
 
-<main>
 
-<h2>Toutes les fiches</h2>
+<section class="fiches">
+
+
+<h2>Nos fiches de révision</h2>
+
 
 <?php
 
-$sql = "
-SELECT fiches.titre, fiches.contenu, matieres.nom_matiere
-FROM fiches
-INNER JOIN matieres
-ON fiches.id_matiere = matieres.id
-";
 
-$resultat = mysqli_query($connexion, $sql);
-
-while($row = mysqli_fetch_assoc($resultat))
+while($fiche = mysqli_fetch_assoc($resultat))
 {
-    echo "<div style='border:1px solid black; padding:10px; margin:10px;'>";
 
-    echo "<h3>" . $row['titre'] . "</h3>";
-    echo "<p><strong>Matière :</strong> " . $row['nom_matiere'] . "</p>";
-    echo "<p>" . $row['contenu'] . "</p>";
 
-    echo "</div>";
+?>
+
+
+<div class="carte">
+
+
+<h3>
+
+<?php echo $fiche['titre']; ?>
+
+</h3>
+
+
+<a href="voir_fiche.php?id=<?php echo $fiche['id']; ?>">
+
+Voir le cours
+
+</a>
+
+
+</div>
+
+
+<?php
+
 }
 
 ?>
 
-</main>
+
+</section>
+
+
+<?php include "includes/footer.php"; ?>
+
 
 </body>
+
 </html>
